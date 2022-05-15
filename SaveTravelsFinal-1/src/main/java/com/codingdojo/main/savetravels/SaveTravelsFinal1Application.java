b529a247935da65e294e6a960cd87ba0d1b945b9
Daniahkhalil <dhkhalil99@gmail.com>
2022-05-15 22:30:55 +0300

    Save Travels updated

---
+++ SaveTravelsFinal-1/src/main/java/com/codingdojo/main/savetravels/SaveTravelsFinal1Application.java
@@ -0,0 +1,13 @@
+package com.codingdojo.main.savetravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveTravelsFinal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveTravelsFinal1Application.class, args);
+	}
+
+}
